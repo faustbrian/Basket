5809e0719d7095bb4285f3b51f97d9b132d544f7
Brian Faust <hello@brianfaust.de>
2016-11-23 10:21:37 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/Transformers/AbstractTransformer.php
+++ src/Transformers/AbstractTransformer.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Basket.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Basket\Transformers;
 
 use BrianFaust\Basket\Contracts\Transformer;
