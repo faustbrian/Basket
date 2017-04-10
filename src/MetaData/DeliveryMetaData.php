5cf683428b2093c4036fb09c79dd2ab11347607a
Brian Faust <hello@brianfaust.de>
2017-04-10 12:42:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/MetaData/DeliveryMetaData.php
+++ src/MetaData/DeliveryMetaData.php
@@ -11,12 +11,20 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Basket.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Basket\MetaData;
 
 use BrianFaust\Basket\Basket;
 use BrianFaust\Basket\Contracts\MetaData;
 use BrianFaust\Basket\Contracts\Reconciler;
-use Money\Currency;
 use Money\Money;
 
 class DeliveryMetaData implements MetaData
