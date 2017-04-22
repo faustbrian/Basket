e71764b60b6d243c2fe5aba7b36b9c6df73ada30
Brian Faust <hello@brianfaust.de>
2017-04-22 08:42:53 +0300

    Try to fix the mess StyleCI makes

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,10 +1,3 @@
 <?php
 
-/*
- * This file is part of Basket.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
