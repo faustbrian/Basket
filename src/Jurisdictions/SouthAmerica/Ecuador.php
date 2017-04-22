2a0b5cde79913ec5e0a5163c7f988633ebd7bffa
Brian Faust <hello@brianfaust.de>
2017-04-22 05:43:10 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Jurisdictions/SouthAmerica/Ecuador.php
+++ src/Jurisdictions/SouthAmerica/Ecuador.php
@@ -3,7 +3,14 @@
 
 declare(strict_types=1);
 
-
+/*
+ * This file is part of Basket.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace BrianFaust\Basket\Jurisdictions\SouthAmerica;
 
